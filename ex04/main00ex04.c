60542d0806eae86e70bb9406a33d8f97525dfc94
Valanna <78416930+Valanna@users.noreply.github.com>
2022-09-19 14:58:16 +0200

    Add files via upload

---
+++ ex04/main00ex04.c
@@ -0,0 +1,12 @@
+void	ft_is_negative(int n);
+
+int	main(void)
+{
+	ft_is_negative(42);
+	ft_is_negative(-10);
+	ft_is_negative(0);
+	ft_is_negative('a');
+	ft_is_negative('\n');
+	//ft_is_negative(a);
+	return(0);
+}
---
+++ ex07/main00ex07.c
@@ -0,0 +1,13 @@
+void ft_putnbr(int nb);
+
+int	main(void)
+{
+	ft_putnbr(42);
+//	ft_putnbr(-123);
+//	ft_putnbr(2147483647);
+//	ft_putnbr(2147483648);
+//	ft_putnbr(-2147483648);
+//	ft_putnbr(-2147483649);
+	return(0);
+}
+
